61431981bc3b4d8f619bda47c024f46881b2a478
Ailen Ramayo <ailen.ramayo.dominguez@gmail.com>
2024-10-27 14:33:06 +0100

    fix: improve locators by css selector

--- src/test/java/com/fashionhub/fwk/web/pages/fashionhub/locators/LoginPageLocators.java
+++ src/test/java/com/fashionhub/fwk/web/pages/fashionhub/locators/LoginPageLocators.java
@@ -5,9 +5,8 @@ public class LoginPageLocators {
 
     private LoginPageLocators() {}
 
-    public static final String LOGIN_ID_FORM = "//form[@id='loginForm']";
-    public static final String USERNAME_INPUT = "//input[@id='username']";
-    public static final String PASSWORD_INPUT = "//input[@id='password']";
+    public static final String USERNAME_INPUT = "#username";
+    public static final String PASSWORD_INPUT = "#password";
     public static final String LOGIN_INPUT_BUTTON = "//form[@id='loginForm']/input[@type='submit']";
-    public static final String INVALID_CREDENTIALS_ERROR_MESSAGE_DIV = "//form[@id='loginForm']/div[@id='errorMessage']" ;
+    public static final String INVALID_CREDENTIALS_ERROR_MESSAGE_DIV = "#errorMessage" ;
 }
--- src/test/java/com/fashionhub/fwk/web/pages/github/locators/PullRequestsPageLocators.java
+++ src/test/java/com/fashionhub/fwk/web/pages/github/locators/PullRequestsPageLocators.java
@@ -4,11 +4,9 @@ public class PullRequestsPageLocators {
 
     private PullRequestsPageLocators() {}
 
-    public static final String PR_TAB_COUNT_SPAN = "//span[@id='pull-requests-repo-tab-count']";
-    public static final String PR_ITEM_LINKS = "//div[contains(@class, 'js-issue-row')]";
+    public static final String PR_TAB_COUNT_SPAN = "#pull-requests-repo-tab-count";
     public static final String PR_ITEM_LINKS_NAMES = "//div[contains(@class, 'js-issue-row')]//a[@data-hovercard-type='pull_request']";
     public static final String PR_ITEM_LINKS_DATES = "//div[contains(@class, 'js-issue-row')]//span[@class='opened-by']/relative-time";
     public static final String PR_ITEM_LINKS_USERS = "//div[contains(@class, 'js-issue-row')]//span[@class='opened-by']/a";
     public static final String PR_NEXT_PAGE_LINK = "//a[@class='next_page']";
-    public static final String PR_DISABLED_NEXT_PAGE_LINK = "//a[contains(@class,'disabled')]";
 }
