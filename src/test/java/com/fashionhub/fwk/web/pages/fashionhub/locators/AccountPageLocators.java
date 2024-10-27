7e8bd060ec261dec32470efe687f9892709b7b21
Ailen Ramayo <ailen.ramayo.dominguez@gmail.com>
2024-10-27 21:33:59 +0100

    add gh_pages

--- src/test/java/com/fashionhub/fwk/web/pages/fashionhub/locators/AccountPageLocators.java
+++
@@ -1,8 +0,0 @@
-package com.fashionhub.fwk.web.pages.fashionhub.locators;
-
-public class AccountPageLocators {
-
-    public static final String DIV_ACCOUNT_PAGE = "//div[@class='account-page']";
-    public static final String WELCOME_MESSAGE_HEADER =  DIV_ACCOUNT_PAGE + "/h2";
-    public static final String LOGOUT_BUTTON =  DIV_ACCOUNT_PAGE + "/logout-button";
-}
